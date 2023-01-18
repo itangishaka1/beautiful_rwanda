017cb2ebd711cdbca901e4e91156aea328fd9016
abdullah <abdullah@abdullahs-MacBook-Air.local>
2023-01-18 14:17:55 -0500

    The easiest way to center anything with transform, top, left.2. Centered our text inside the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
-import './Header.scss';
+import "./Header.scss";
+import Logo from "../../img/logo-white.png";
 
 const Header = () => {
   return (
-    <header className='header'>Header</header>
-  )
-}
+    <header className="header">
+      <div className="logo-box">
+        <img src={Logo} alt="Logo" className="logo" />
+      </div>
 
-export default Header
+      <div className="text-box">
+        <h1 className="heading-primary">
+          <span className="heading-primary-main">visit rwanda</span>
+          <span className="heading-primary-sub">the country of 1000 hills</span>
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
