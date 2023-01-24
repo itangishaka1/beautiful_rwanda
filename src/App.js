e18ea602e31c2f23223c94964f9ce29258a6f475
itangishaka1 <abdullahitangishaka@gmail.com>
2023-01-24 11:24:20 -0500

    1. Create a grid layout with float 2. Using attribute selector 3. Using :not pseudo class 4. Using calc() 5. Add <GridTest /

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import GridTest from './components/Grid-test/GridTest'
 
 function App() {
   return (
     <>
       <Header />
+      <GridTest />
     </>
   );
 }
