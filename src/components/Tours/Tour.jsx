cfe79796d514ae5c85a4fd353da0703f50175ec3
itangishaka1 <abdullahitangishaka@gmail.com>
2023-01-30 14:46:55 -0500

    Building the tours section front side

--- src/components/Tours/Tour.jsx
+++ src/components/Tours/Tour.jsx
@@ -1,11 +1,25 @@
 import './Tour.scss'
 
-const Tour = ({ card_number }) => {
+const Tour = ({ card_number , heading, picture_number, heading_span, item1, item2, item3, item4, item5}) => {
   return (
     <article className='row'>
         <div className="col-1-of-3">
             <div className="card">
-                <div className="card__side card__side--front">Front side</div>
+                <div className="card__side card__side--front">
+                    <div className={`card__picture ${picture_number}`}>&nbsp;</div>
+                    <h4 className="card__heading">
+                        <span className={`card__heading-span ${heading_span}`}>{heading}</span>    
+                    </h4>
+                    <div className="card__details">
+                        <ul>
+                            <li>{item1}</li>
+                            <li>{item2}</li>
+                            <li>{item3}</li>
+                            <li>{item4}</li>
+                            <li>{item5}</li>
+                        </ul>
+                    </div>
+                </div>
                 <div className={`card__side card__side--back ${card_number}`}>Back side</div>
             </div>
         </div>
--- src/components/Tours/Tours.jsx
+++ src/components/Tours/Tours.jsx
@@ -7,7 +7,21 @@ const Tours = () => {
       <div className="u-center-text u-margin-bottom-big">
         <h2 className="section-heading">most popular tours</h2>
       </div>
-      <Tour  card_number='card__side--back--1' />
+      <Tour  
+            // Front side
+            picture_number='card__picture--1'
+            heading='the sea explorer' 
+            heading_span='card__heading-span--1'
+            details='details'
+            item1 = '3 day tours'
+            item2 = 'Up to 30 people'
+            item3 = '2 tour guides'
+            item4 = 'sleep in cozy hotels'
+            item5 = 'Difficulty: easy'
+
+            // Back side
+            card_number='card__side--back--1' 
+        />
     </section>
   );
 };
