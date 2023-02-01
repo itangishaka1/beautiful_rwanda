0c708e3df09f2cd21a6253a8e9f1ae7c7aab3b53
itangishaka1 <abdullahitangishaka@gmail.com>
2023-02-01 00:27:56 -0500

    Building the tours section back side

--- src/components/Tours/Tour.jsx
+++ src/components/Tours/Tour.jsx
@@ -1,32 +1,48 @@
-import './Tour.scss'
+import Button from "../Button/Button";
+import "./Tour.scss";
 
-const Tour = ({ card_number , heading, picture_number, heading_span, item1, item2, item3, item4, item5}) => {
+const Tour = ({
+  card_number,
+  heading,
+  picture_number,
+  heading_span,
+  item1,
+  item2,
+  item3,
+  item4,
+  item5,
+  price_value,
+}) => {
   return (
-    <article className='row'>
-        <div className="col-1-of-3">
-            <div className="card">
-                <div className="card__side card__side--front">
-                    <div className={`card__picture ${picture_number}`}>&nbsp;</div>
-                    <h4 className="card__heading">
-                        <span className={`card__heading-span ${heading_span}`}>{heading}</span>    
-                    </h4>
-                    <div className="card__details">
-                        <ul>
-                            <li>{item1}</li>
-                            <li>{item2}</li>
-                            <li>{item3}</li>
-                            <li>{item4}</li>
-                            <li>{item5}</li>
-                        </ul>
-                    </div>
-                </div>
-                <div className={`card__side card__side--back ${card_number}`}>Back side</div>
-            </div>
+    <article className="card">
+      <div className="card__side card__side--front">
+        <div className={`card__picture ${picture_number}`}>&nbsp;</div>
+        <h4 className="card__heading">
+          <span className={`card__heading-span ${heading_span}`}>
+            {heading}
+          </span>
+        </h4>
+        <div className="card__details">
+          <ul>
+            <li>{item1}</li>
+            <li>{item2}</li>
+            <li>{item3}</li>
+            <li>{item4}</li>
+            <li>{item5}</li>
+          </ul>
         </div>
-        <div className="col-1-of-3">col 1 of 3</div>
-        <div className="col-1-of-3">col 1 of 3</div>
+      </div>
+      <div className={`card__side card__side--back ${card_number}`}>
+        <div className="card__cta">
+          <div className="card__price-box">
+            <p className="card__price-only">Only</p>
+            <p className="card__price-value">${price_value}</p>
+          </div>
+          <Button className="btn--white" text="Book now!" />
+        </div>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default Tour
+export default Tour;
--- src/components/Tours/Tours.jsx
+++ src/components/Tours/Tours.jsx
@@ -1,5 +1,6 @@
 import "./Tours.scss";
-import Tour from './Tour'
+import Tour from "./Tour";
+import Button from "../Button/Button";
 
 const Tours = () => {
   return (
@@ -7,21 +8,69 @@ const Tours = () => {
       <div className="u-center-text u-margin-bottom-big">
         <h2 className="section-heading">most popular tours</h2>
       </div>
-      <Tour  
-            // Front side
-            picture_number='card__picture--1'
-            heading='the sea explorer' 
-            heading_span='card__heading-span--1'
-            details='details'
-            item1 = '3 day tours'
-            item2 = 'Up to 30 people'
-            item3 = '2 tour guides'
-            item4 = 'sleep in cozy hotels'
-            item5 = 'Difficulty: easy'
 
+      <article className="row">
+        <div className="col-1-of-3">
+          <Tour
+            // Front side
+            picture_number="card__picture--1"
+            heading="the sea explorer"
+            heading_span="card__heading-span--1"
+            details="details"
+            item1="3 day tours"
+            item2="Up to 30 people"
+            item3="2 tour guides"
+            item4="sleep in cozy hotels"
+            item5="Difficulty: easy"
             // Back side
-            card_number='card__side--back--1' 
-        />
+            card_number="card__side--back--1"
+            price_value="299"
+          />
+        </div>
+
+        <div className="col-1-of-3">
+          <Tour
+            // Front side
+            picture_number="card__picture--2"
+            heading="the forest hiker"
+            heading_span="card__heading-span--2"
+            details="details"
+            item1="4 day tours"
+            item2="Up to 40 people"
+            item3="6 tour guides"
+            item4="sleep in provided"
+            item5="Difficulty: medium"
+            // Back side
+            card_number="card__side--back--2"
+            price_value="399"
+          />
+        </div>
+
+        <div className="col-1-of-3">
+          <Tour
+            // Front side
+            picture_number="card__picture--3"
+            heading="the snow adventurer"
+            heading_span="card__heading-span--3"
+            details="details"
+            item1="5 day tours"
+            item2="Up to 15 people"
+            item3="3 tour guides"
+            item4="sleep in provided tents"
+            item5="Difficulty: hard"
+            // Back side
+            card_number="card__side--back--3"
+            price_value="599"
+          />
+        </div>
+      </article>
+      <div className="u-center-text u-margin-top-huge">
+      <Button 
+       text='discover all tours'
+       className='btn--green'
+       />
+       </div>
+       
     </section>
   );
 };
