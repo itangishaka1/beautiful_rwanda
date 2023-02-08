4bf96aec825c9f457d3749d29827348798819504
itangishaka1 <abdullahitangishaka@gmail.com>
2023-02-07 23:20:15 -0500

    Building navigation part 3 (the button)

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,7 +4,9 @@ const Navigation = () => {
     return(
         <div className="navigation">
             <input type="checkbox" className="navigation__checkbox" id='nav-toggle' />
-            <label htmlFor="nav-toggle" className="navigation__button">Menu</label>
+            <label htmlFor="nav-toggle" className="navigation__button">
+                <span className="navigation__icon">&nbsp;</span>
+            </label>
             <div className="navigation__background">&nbsp;</div>
             <nav className="navigation__nav">
                 <ul className="navigation__list">
